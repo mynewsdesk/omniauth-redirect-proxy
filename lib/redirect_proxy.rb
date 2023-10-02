9cde47d7a76933b5c92cc96bef764e3ecbc6fc43
David Backeus <duztdruid@gmail.com>
2023-10-02 12:58:32 +0200

    State parameter error handling

--- lib/redirect_proxy.rb
+++ lib/redirect_proxy.rb
@@ -7,8 +7,11 @@ class RedirectProxy
   def call(env)
     request = Rack::Request.new(env)
 
+    # abort unless request.params["state"] is passed
+    return [400, {}, ["Bad Request: Missing state parameter"]] unless request.params["state"]
+
     # Eg. https://your-puma-dev-app.test or https://your-staging-environment.com etc
-    origin_uri = URI.parse(Base64.decode64(request.params["state"]))
+    origin_uri = URI.parse(Base64.strict_decode64(request.params["state"]))
 
     # We redirect with the path / query string from the incoming request but replace the
     # host, scheme, and port with the origin_uri from the state parameter.
@@ -18,5 +21,13 @@ class RedirectProxy
     redirect_uri.port = origin_uri.port
 
     [301, { "Location" => redirect_uri.to_s }, []]
+  rescue ArgumentError => e
+    if e.message == "invalid base64"
+      [400, {}, ["Bad Request: State parameter must be a valid Base64 encoded string"]]
+    else
+      raise
+    end
+  rescue URI::InvalidURIError
+    [400, {}, ["Bad Request: State parameter must be a valid Base64 encoded URI"]]
   end
 end
